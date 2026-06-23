b620f7c723bc38412f369862004ada86e6faaa55
Aryan Veen <145652108+aryanveen05@users.noreply.github.com>
2026-06-23 19:10:25 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -16,20 +16,25 @@ const gameName = new String('aryan-av-com')
 // console.log(gameName.charAt(2));
 // console.log(gameName.indexOf('a'));
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
 
-const anotherString = gameName.slice(-8, 6)
-console.log(anotherString);
+// const anotherString = gameName.slice(-8, 6)
+// console.log(anotherString);
 
-const newStringOne = "   aryan    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// const newStringOne = "   aryan    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
-const url = "https://aryan.com/aryan%05veen"
+// const url = "https://aryan.com/aryan%05veen"
 
-console.log(url.replace('%05', '-'))
+// console.log(url.replace('%05', '-'))
 
-console.log(url.includes('veen'))
+// console.log(url.includes('veen'))
 
-console.log(gameName.split('-'))
+// console.log(gameName.split('-'))
+
+const str1 = "Hello";
+const str2 = "World";
+
+console.log(str1.concat(" ", str2));
